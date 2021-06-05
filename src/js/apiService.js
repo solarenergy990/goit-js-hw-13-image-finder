6841d0f89f9be11a4e4e364adc35a4e54a952018
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-06-05 18:55:46 +0200

    adds async function

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,18 +8,32 @@ export default class ImagesApiService {
     this.perPage = 12;
   }
 
-  fetchImages() {
-    console.log(this);
-    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${API_KEY}`;
+  // fetchImages() {
+  //   console.log(this);
+  //   const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${API_KEY}`;
 
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data.hits);
-        this.incrementPage();
+  //   return fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       // console.log(data.hits);
+  //       this.incrementPage();
 
-        return data.hits;
-      });
+  //       return data.hits;
+  //     });
+  // }
+
+  async fetchImages() {
+    const url = await fetch(
+      `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${API_KEY}`,
+    );
+
+    const data = await url.json().then(data => {
+      this.incrementPage();
+
+      return data;
+    });
+
+    return data.hits;
   }
 
   incrementPage() {
